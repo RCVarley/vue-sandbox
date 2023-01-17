ae774625b631207b7cad7ed3829f2690af620cbf
Ryan Varley <rcvarley@outlook.com>
2023-01-17 16:42:44 +0000

    tidied up codes store

--- src/stores/codes.ts
+++ src/stores/codes.ts
@@ -1,13 +1,20 @@
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
-import * as Codes from '@/assets/codesMap.json';
+import Codes from '../assets/codesMap.json';
 
-type CodesMap = [string, (string | number)[][]][];
-const c: CodesMap = Codes as CodesMap;
+type CodesData = [string, (string | number)[][]][];
+type CodesMap = Map<string, (string | number)[][]>;
+type ReadonlyCodesMap = ReadonlyMap<string, (string | number)[][]>;
+
+const codesMap: CodesMap = new Map<string, (string | number)[][]>(
+  Codes as CodesData
+);
 
 export const useCodesStore = defineStore('codes', () => {
-  const sourceCodes = ref(new Map<string, (string | number)[][]>(c));
-  const getSourceCodes = computed(() => sourceCodes);
+  const sourceCodes = ref(codesMap);
+  const getSourceCodes = computed<ReadonlyCodesMap>(
+    () => sourceCodes.value as ReadonlyCodesMap
+  );
 
   return { sourceCodes, getSourceCodes };
 });
