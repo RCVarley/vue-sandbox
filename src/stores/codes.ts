ff1d24b0a53004864a65fb21e4ec8e7f5cb437db
Ryan Varley <rcvarley@outlook.com>
2023-01-19 16:56:38 +0000

    using records instead of map for stored data

--- src/stores/codes.ts
+++ src/stores/codes.ts
@@ -1,19 +1,11 @@
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
-import Codes from '../assets/codesMap.json';
-
-type CodesData = [string, (string | number)[][]][];
-type CodesMap = Map<string, (string | number)[][]>;
-type ReadonlyCodesMap = ReadonlyMap<string, (string | number)[][]>;
-
-const codesMap: CodesMap = new Map<string, (string | number)[][]>(
-  Codes as CodesData
-);
+import { computed, ref, type Ref } from 'vue';
+import Codes from '../assets/codesObject.json';
 
 export const useCodesStore = defineStore('codes', () => {
-  const sourceCodes = ref(codesMap);
-  const getSourceCodes = computed<ReadonlyCodesMap>(
-    () => sourceCodes.value as ReadonlyCodesMap
+  const sourceCodes: Ref<Record<string, (string | number)[][]>> = ref(Codes);
+  const getSourceCodes = computed<Record<string, (string | number)[][]>>(
+    () => sourceCodes.value
   );
 
   return { sourceCodes, getSourceCodes };
