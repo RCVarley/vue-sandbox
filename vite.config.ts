e751492db63ecaa989c720d06b48f7430de89322
Ryan Varley <rcvarley@outlook.com>
2023-01-20 14:30:35 +0000

    added custom tags to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            tag === 'el-stack' ||
+            tag === 'el-box' ||
+            tag === 'el-center' ||
+            tag === 'el-cluster' ||
+            tag === 'el-sidebar' ||
+            tag === 'el-switcher' ||
+            tag === 'el-cover' ||
+            tag === 'el-grid' ||
+            tag === 'el-frame' ||
+            tag === 'el-reel' ||
+            tag === 'el-imposter' ||
+            tag === 'el-icon',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
